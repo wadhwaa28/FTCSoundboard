9794ba4d78487b8b7181015469533248529e3983
wadhwaa28 <88746385+wadhwaa28@users.noreply.github.com>
2025-02-12 20:00:30 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+// Sound file paths (now using .wav format)
+const sounds = {
+    sound1: new Audio('match_start.wav'),
+    sound2: new Audio('sounds/sound2.wav'),
+    sound3: new Audio('sounds/sound3.wav'),
+    sound4: new Audio('sounds/sound4.wav'),
+    sound5: new Audio('sounds/sound5.wav'),
+};
+
+// Function to play an individual sound
+function playSound(soundId) {
+    sounds[soundId].play();
+}
+
+// Function to play all sounds in sequence
+function playAllSounds() {
+    const soundIds = ['sound1', 'sound2', 'sound3', 'sound4', 'sound5'];
+    let index = 0;
+
+    function playNextSound() {
+        if (index < soundIds.length) {
+            sounds[soundIds[index]].play();
+            sounds[soundIds[index]].onended = () => {
+                index++;
+                playNextSound();
+            };
+        }
+    }
+
+    playNextSound();
+}
+
+// Attach event listeners to buttons
+document.getElementById('sound1').addEventListener('click', () => playSound('sound1'));
+document.getElementById('sound2').addEventListener('click', () => playSound('sound2'));
+document.getElementById('sound3').addEventListener('click', () => playSound('sound3'));
+document.getElementById('sound4').addEventListener('click', () => playSound('sound4'));
+document.getElementById('sound5').addEventListener('click', () => playSound('sound5'));
+document.getElementById('sequence').addEventListener('click', playAllSounds);
